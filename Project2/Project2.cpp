1c27e6c0635da647f8d5fbea681399d1dbad2b10
Chihow <546786003@qq.com>
2022-08-14 17:15:16 +0800

    update help list

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -4,6 +4,9 @@
 #include <unistd.h>     //for parse short option
 #include <iostream>
 
+#define RED "\033[31m"
+#define RESET "\033[0m"
+
 std::string filename = "sample.txt";
 std::string oper[2];
 
@@ -11,7 +14,9 @@ void print_usage(){
     std::cout << "Usage: add [OPTION]... [FILE]...\n"
               << "Deal with long integer addition from file input.\n"
               << "Options:\n" 
-              << "\t-i  set input file path\n"
+              << "\t-f  set input file path\n"
+              << "\t-i  set input base format\n"
+              << "\t-o  set output base format\n"
               << "\t-h  give this help list\n";
 }
 
@@ -19,7 +24,7 @@ bool read_operator(const std::string& fn, std::string* op){
     std::ifstream fin;
     fin.open(fn, std::ios::in);
     if (!fin.is_open()){
-        std::cout << "[ERROR]: Open file failed!\n";
+        std::cerr << RED << "[ERROR]: Open file failed!\n" << RESET;
         return false;
     }
     std::getline(fin, *op);
@@ -31,16 +36,28 @@ bool read_operator(const std::string& fn, std::string* op){
 
 void run(int argc, char* argv[]){
     int opt = 0;
-    while((opt = getopt(argc, argv, "hi:")) != -1){
+    int flag_i = 10, flag_o = 10;
+    while((opt = getopt(argc, argv, "f:hi:o:")) != -1){
         switch (opt)
         {
         case 'h':
             print_usage();
             exit(0);
-            break;
-        case 'i':
+        case 'f':
             filename = optarg;
             break;
+        case 'i':
+            flag_i = static_cast<int>(*optarg - '0');
+            std::cout << *optarg << std::endl;
+            break;
+        case 'o':
+            flag_o = static_cast<int>(*optarg - '0');
+            std::cout << *optarg << std::endl;
+            break;
+        case '?':
+            std::cerr << RED << "[ERROR]: Invalid option!\n" << RESET;
+            print_usage(); 
+            exit(1);
         }
     }
     read_operator(filename, oper);
